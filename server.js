411797f48da3fc91928385e23003efd691d9ed5d
Dr.Worm <sky.jackson@icloud.com>
2022-05-15 19:36:24 -0400

    Add Heroku

--- server.js
+++ server.js
@@ -3,8 +3,44 @@ const express = require('express');
 
 const app = express();
 
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+        // If personalityTraits is a string, place it into a new array and save
+        if (typeof query.personalityTraits === 'string') {
+            // If has multiple personality traits, use array of those.
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            // If has only 1 personality trait, use string.
+            personalityTraitsArray = query.personalityTraits;
+        }
+        // Loop through each trait in the perosnailtyTraits array:
+        personalityTraitsArray.forEach(trait => {
+            // Check the trait against each animal in the filteredResults array. For each trait being targeted by the filter, the filteredResults array will then contain only the entries that contain the trait, so at the end we'll have an array of animals that have every one of the traits when the .forEach() loop is finished.
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+        );
+        });
+    }
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+};
+
 app.get('/api/animals', (req, res) => {
-    res.json(animals);
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
 });
 
 app.listen(3001, () => {
